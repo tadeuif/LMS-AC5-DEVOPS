658a33f00bd22a59c5b2d8bd64e047feb059415d
nathaliaescarlate <nathaliaescarlate@hotmail.com>
2018-09-26 23:11:23 -0300

    Novas operações Python

--- funcoes.py
+++ funcoes.py
@@ -5,3 +5,12 @@ def funcaosoma(n1,n2):
 def funcaosubtrai(n1,n2):
     resp=n1-n2
     return resp
+
+def funcaomultiplica(n1,n2):
+    resp = n1*n2
+    return resp
+
+def funcaoexpo(n1,n2):
+    resp = n1**n2
+    return resp
+    
