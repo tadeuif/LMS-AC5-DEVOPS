d5713f294dedf358e81e377ea49dcc8139181b48
viniciusmottag <vinicius.mottag@gmail.com>
2018-09-26 21:22:12 -0300

    Ajuste arquivos py

---
+++ Main.py
@@ -0,0 +1,3 @@
+from funcoes import *
+print("A soma de 5+2 é:",funcaosoma(2,3))
+print("A subtração de 3-1 é: ",funcaosubtrai(3,2))
---
+++ funcoes.py
@@ -0,0 +1,7 @@
+def funcaosoma(n1,n2):
+    resp=n1+n2
+    return resp
+
+def funcaosubtrai(n1,n2):
+    resp=n1-n2
+    return resp
